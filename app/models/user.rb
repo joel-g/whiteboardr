f271b632b5f7a4a00a174a15d2a3715092eff477
Jordan Levine & Jeremy Lee <pair+j0rdania.pyrofist@devbootcamp.com>
2017-07-27 16:36:26 -0700

    Add basic associations and validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates_presence_of :email, :username
+  validates_uniqueness_of :email, :username
+
+  has_many :interviews, class_name: 'Interview', foreign_key: 'applicant_id'
+  has_many :feedbacks, through: :interviews
 end
