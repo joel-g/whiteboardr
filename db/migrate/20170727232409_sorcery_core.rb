fd455a66a09c2e31cec3baad6155bbf15031eb2c
Jordan Levine & Jeremy Lee <pair+j0rdania.pyrofist@devbootcamp.com>
2017-07-27 16:32:02 -0700

    Update sorcery generated migration with required user fields, drop old user table

--- db/migrate/20170727232409_sorcery_core.rb
+++ db/migrate/20170727232409_sorcery_core.rb
@@ -1,9 +1,13 @@
 class SorceryCore < ActiveRecord::Migration[5.0]
   def change
+    drop_table :users
+    
     create_table :users do |t|
       t.string :email,            :null => false
       t.string :crypted_password
       t.string :salt
+      t.string :username, null: false
+      t.string :name, null: false
 
       t.timestamps                :null => false
     end
