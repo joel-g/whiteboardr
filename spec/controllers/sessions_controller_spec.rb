40f2c56c923e4cabbc8893bce019796d30d662a2
Kris & Jeremy Lee <pair+knweber.pyrofist@devbootcamp.com>
2017-07-28 15:03:36 -0700

    Write tests for sessions#destroy and invalid sessions#create

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -9,8 +9,8 @@ describe SessionsController do
   end
 
   context '#create' do
+    let!(:user) { FactoryGirl.create(:user, password: 'bob') }
     context 'valid user' do
-      let!(:user) { FactoryGirl.create(:user, password: 'bob') }
       it 'redirects to root when login successful' do
         post :create, params: { email: user.email, password: 'bob' }
         expect(response).to redirect_to(root_path)
@@ -21,6 +21,33 @@ describe SessionsController do
       end
     end
     context 'invalid user' do
+      it 'renders the new page' do
+        post :create, params: { email: user.email, password: '' }
+        expect(response).to render_template('new')
+      end
+      it 'returns an alert informing the user of failed login' do
+        post :create, params: { email: user.email, password: '' }
+        expect(flash[:alert]).to match 'Invalid email or password'
+      end
+      it 'doesn\'t logs in the user' do
+        post :create, params: { email: user.email, password: '' }
+        expect(logged_in?).to be false
+      end
+    end
+  end
+
+  context '#destroy' do
+    before(:each) do
+      user = FactoryGirl.create(:user, password: 'bob')
+      post :create, params: { email: user.email, password: 'bob' }
+    end
+    it 'redirects to root' do
+      delete :destroy, params: { id: 1 }
+      expect(response).to redirect_to root_path
+    end
+    it 'logs out the user' do
+      delete :destroy, params: { id: 1 }
+      expect(logged_in?).to be false
     end
   end
 
