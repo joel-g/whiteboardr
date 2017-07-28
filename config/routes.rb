0d61ec389db482b6a02b6afa6b07a59233b98f96
Jordan Levine & Jeremy Lee <pair+j0rdania.pyrofist@devbootcamp.com>
2017-07-28 10:07:11 -0700

    Restrict user routes, add session routes, add aliases for login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
     resources :feedbacks, only: [:create]
   end
 
-  resources :users
+  resources :users, only: [:new, :create, :show]
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'login' => 'sessions#new'
+  delete 'logout' => 'sessions#destroy'
+
 
 end
