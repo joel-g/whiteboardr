8b67ac8554dc54799b81ce7ec9ac994cbf4adc79
Jordan Levine & Jeremy Lee <pair+j0rdania.pyrofist@devbootcamp.com>
2017-07-27 17:09:37 -0700

    Create example user for uniqueness validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,5 @@
-require 'spec_helper'
+require 'rails_helper'
+
 
 describe User, type: :model do
   context 'validations' do
@@ -6,6 +7,7 @@ describe User, type: :model do
     it { is_expected.to validate_presence_of(:username) }
     it { is_expected.to validate_presence_of(:first_name) }
     it { is_expected.to validate_presence_of(:last_name) }
+    subject { FactoryGirl.build(:user) }
     it { is_expected.to validate_uniqueness_of(:email)}
     it { is_expected.to validate_uniqueness_of(:username)}
   end
