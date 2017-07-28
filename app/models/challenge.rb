fc3da3ce4eb3ac1136c690ded516d3c0837da6b4
Joel Guerra & Kris <pair+joel-g.knweber@devbootcamp.com>
2017-07-28 10:43:03 -0700

    add challenge#new view

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,7 +1,6 @@
 class Challenge < ActiveRecord::Base
   validates :title, presence: true
-  validates :input, presence: true
-  validates :output, presence: true
   validates :body, presence: true
+  validates :difficulty, presence: true
   belongs_to :user
 end
