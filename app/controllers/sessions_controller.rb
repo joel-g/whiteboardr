af8bb315df644c9d8667c44b0d0115c1c131ebfa
Kris & Jeremy Lee <pair+knweber.pyrofist@devbootcamp.com>
2017-07-28 14:37:28 -0700

    implement sessions#create action and display error to user upon failed login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,17 @@
 class SessionsController < ApplicationController
 
   def new
-    render :new  
+    render :new
   end
 
   def create
+    @user = login(params[:email], params[:password])
+    if @user
+      redirect_to root_path
+    else
+      flash.now[:alert] = 'Invalid email or password'
+      render :new
+    end
   end
 
   def destroy
