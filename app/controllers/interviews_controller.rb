dc4dd39fce359490d969ed2a890c4d55a5efc06e
Jordan Levine & Joel Guerra <pair+j0rdania.joel-g@devbootcamp.com>
2017-07-28 16:07:39 -0700

    Refactor interview#create and handle no challenge selected

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -6,11 +6,12 @@ class InterviewsController < ApplicationController
 
   def new
     @interview = Interview.new
-    @challenges = Challenge.all.order(difficulty: 'ASC', title: 'ASC')
+    populate_challenges
   end
 
   def create
     @interview = Interview.new
+    entered_applicant_name = params[:interview][:applicant_id]
     applicant = User.find_by(username: params[:interview][:applicant_id])
     if applicant
       params[:interview][:applicant_id] = applicant.id
@@ -18,24 +19,27 @@ class InterviewsController < ApplicationController
       if @interview.save
         redirect_to interview_path(@interview.id)
       else
-        @challenges = Challenge.all.order(difficulty: 'ASC', title: 'ASC')
+        populate_challenges
         @interview.errors.add(:interview, 'was not started.')
+        @interview.applicant_id = entered_applicant_name
         render :new
       end
     else
-      @challenges = Challenge.all.order(difficulty: 'ASC', title: 'ASC')
+      populate_challenges
       @interview.errors.add(:applicant, 'username not found.')
       render :new
     end
-
   end
-
 
   def show
    @interview = Interview.find(params[:id])
   end
 
   private
+
+  def populate_challenges
+    @challenges = Challenge.all.order(difficulty: 'ASC', title: 'ASC')
+  end
 
   def interview_params
     params.require(:interview).permit(:interviewer_id, :applicant_id, :challenge_id)
