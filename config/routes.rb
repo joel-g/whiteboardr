eea4e8473741bb5690a5fd74e892fba16fe05754
Joel Guerra & Kris <pair+joel-g.knweber@devbootcamp.com>
2017-07-28 10:05:01 -0700

    adds interview#index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :challenges, only: [:create, :new]
-  resources :interviews, only: [:new, :create, :show] do
+  resources :interviews, only: [:index, :new, :create, :show] do
     resources :feedbacks, only: [:create]
   end
 
