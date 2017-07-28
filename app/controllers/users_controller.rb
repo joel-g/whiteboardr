80ecba8304c811c48851e2af30bb47bcc7f75105
Jordan Levine & Jeremy Lee <pair+j0rdania.pyrofist@devbootcamp.com>
2017-07-28 11:47:48 -0700

    Implement user#create action with error handling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,23 @@ class UsersController < ApplicationController
     @user = User.new
     render :new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      if @user = login(@user.email, params[:user][:password])
+        redirect_to root_path
+      else
+        flash.now[:alert] = 'Login failed'
+        render :new
+      end
+    else
+      render :new
+    end
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :username, :password)
+  end
 end
