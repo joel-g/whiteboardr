50c64f5524bf537216451494919172816a294754
Jordan Levine & Jeremy Lee <pair+j0rdania.pyrofist@devbootcamp.com>
2017-07-28 12:30:57 -0700

    Define tests for User#create action

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,80 @@
+require 'rails_helper'
+
+describe UsersController do
+  context '#new' do
+    it 'renders the User#new view' do
+      get :new
+      expect(response).to render_template('new')
+    end
+
+    it 'assigns @user to be a new User' do
+      get :new
+      expect(assigns(:user)).to be_a_new(User)
+    end
+  end
+
+  context '#create' do
+    let!(:valid_user_attributes) { FactoryGirl.attributes_for(:user) }
+    context 'valid user information' do
+      it 'redirects to root when login successful' do
+        post :create, params: { user: valid_user_attributes }
+        expect(response).to redirect_to(root_path)
+      end
+
+      it 'creates a user' do
+        user_count = User.all.count
+        post :create, params: { user: valid_user_attributes }
+        expect(User.all.count).to eq(user_count + 1)
+      end
+
+      it 'logs in the user' do
+        post :create, params: { user: valid_user_attributes }
+        expect(logged_in?).to be true
+      end
+    end
+
+    context 'invalid user information' do
+      it 'does not save a user with no email' do
+        missing_email = FactoryGirl.attributes_for(:user, email: '')
+        post :create, params: { user: missing_email }
+        expect(assigns[:user].errors).not_to be_empty
+      end
+      missing_first_name = FactoryGirl.attributes_for(:user, first_name: '')
+      it 'does not save a user with no first_name' do
+        post :create, params: { user: missing_first_name }
+        expect(assigns[:user].errors).not_to be_empty
+      end
+      it 'does not save a user with no last_name' do
+        missing_last_name = FactoryGirl.attributes_for(:user, last_name: '')
+        post :create, params: { user: missing_last_name }
+        expect(assigns[:user].errors).not_to be_empty
+      end
+      it 'does not save a user with no username' do
+        missing_username = FactoryGirl.attributes_for(:user, username: '')
+        post :create, params: { user: missing_username }
+        expect(assigns[:user].errors).not_to be_empty
+      end
+      xit 'does not save a user with no password' do
+        missing_password = FactoryGirl.attributes_for(:user, password: '')
+        post :create, params: { user: missing_password }
+        expect(assigns[:user].errors).not_to be_empty
+      end
+      it 'does not save a user with a duplicate username' do
+        user = FactoryGirl.create(:user)
+        duplicate_username = FactoryGirl.attributes_for(:user, username: user.username)
+        post :create, params: { user: duplicate_username }
+        expect(assigns[:user].errors).not_to be_empty
+      end
+      it 'does not save a user with a duplicate email' do
+        user = FactoryGirl.create(:user)
+        duplicate_email = FactoryGirl.attributes_for(:user, email: user.email)
+        post :create, params: { user: duplicate_email }
+        expect(assigns[:user].errors).not_to be_empty
+      end
+      it 'shows the #new view' do
+        post :create, params: { user: { type: 'invalid' } }
+        expect(response).to render_template('new')
+      end
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'sorcery'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -34,6 +35,8 @@ Shoulda::Matchers.configure do |config|
 end
 
 RSpec.configure do |config|
+  config.include Sorcery::TestHelpers::Rails::Controller
+  config.include Sorcery::TestHelpers::Rails::Integration
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
