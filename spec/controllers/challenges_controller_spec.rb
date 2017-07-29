f55aa3ed9eefdea6c8c95473b83f6baf48443327
Jeremy Lee <email": null>
2017-07-29 16:19:18 -0700

    Rewrite ChallengesController#new specs

--- spec/controllers/challenges_controller_spec.rb
+++ spec/controllers/challenges_controller_spec.rb
@@ -2,18 +2,26 @@ require 'rails_helper'
 
 describe ChallengesController do
   describe '#new' do
-    it 'responds with a status of 302 if user is not logged in' do
-      get :new
-      expect(response.status).to eq 302
+    context 'Logged in' do
+      before(:each) do
+        @user = FactoryGirl.create(:user)
+        login_user
+        get :new
+      end
+      it 'assigns @challenge to a new challenge' do
+        expect(assigns(:challenge)).to be_a_new(Challenge)
+      end
+      it 'Renders the new page' do
+        expect(response).to render_template :new
+      end
     end
-    it 'redirects to login page if user is not logged in' do
-      get :new
-      expect(response).to redirect_to login_path
+    context 'Logged out' do
+      it 'redirects to login page if user is not logged in' do
+        get :new
+        expect(response).to redirect_to login_path
+      end
     end
-    xit 'response with a status of 200 if user is logged in' do
 
-      expect(response.status).to eq 200
-    end
   end
 
   xdescribe '#create' do
