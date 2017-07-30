f667b859d93343065c77a656e9669b50fdc1af60
Joel Guerra <joelaguerra1@gmail.com>
2017-07-30 11:32:11 -0700

    defines migrations/model for Tag and Challenge_Tag, changes Challenge migration

--- db/migrate/20170730182906_create_challenge_tags.rb
+++ db/migrate/20170730182906_create_challenge_tags.rb
@@ -1,7 +1,8 @@
 class CreateChallengeTags < ActiveRecord::Migration[5.0]
   def change
     create_table :challenge_tags do |t|
-
+      t.integer :tag_id, null: false
+      t.integer :challenge_id, null: false
       t.timestamps
     end
   end
