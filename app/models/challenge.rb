266c2fe69c8737459e6681803ae328aea465d6c7
Joel Guerra <joelaguerra1@gmail.com>
2017-07-30 11:42:01 -0700

    Add associations and validations to Challenge_Tag and Tag

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,8 +1,9 @@
 class Challenge < ActiveRecord::Base
-  validates :title, presence: true
-  validates :body, presence: true
+  validates :title, presence: true, uniqueness: true
+  validates :body, presence: true, uniqueness: true
   validates :difficulty, presence: true
   belongs_to :user
+  has_many :tags, through: :challenge_tags
 
   def title_and_difficulty
     "#{self.title} - #{self.difficulty}"
