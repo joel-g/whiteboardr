a18ee4dd60ad4a52bec144f59aa88edb0c5f0e05
Joel Guerra <joelaguerra1@gmail.com>
2017-07-30 13:25:05 -0700

    Write tests for #isolate_tags

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,4 +1,4 @@
 def isolate_tags(string_of_tags)
   tag_array = string_of_tags.split(',').map {|each| each.strip}
-  tag_array.reject! {|each| each == ''}
+  tag_array.reject {|each| each.empty? }
 end
---
+++ spec/helper_spec.rb
@@ -0,0 +1,12 @@
+require_relative "../app/helpers/helpers.rb"
+
+describe 'helpers' do
+  context '#isolate_tags' do
+    it 'takes of a string of commma seperated tags and returns an array of tags' do
+      expect(isolate_tags('hello, hi , , , , how, are, you')).to eq ['hello', 'hi', 'how', 'are', 'you']
+    end
+    it 'takes of a string of a single tag and returns an array of a single tag' do
+      expect(isolate_tags('muggle')).to eq ['muggle']
+    end
+  end
+end
