29477b25df87eb5fd0370e4803c1adc95e1436ae
Jeremy Lee <email": null>
2017-07-29 17:11:08 -0700

    Exclude Rails generated files from SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,12 @@
 # it.
 #
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do
+  add_filter 'app/channels/application_cable/channel.rb'
+  add_filter 'app/channels/application_cable/connection.rb'
+  add_filter 'app/jobs/application_job.rb'
+  add_filter 'app/mailers/application_mailer.rb'
+end
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
