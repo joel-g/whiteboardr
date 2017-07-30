c26844f12cbd29c84bda4bed20663e6e9ca8375c
Jordan Levine & Jeremy Lee <pair+j0rdania.pyrofist@devbootcamp.com>
2017-07-30 15:33:43 -0700

    Add unique constraint to challenge title

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,5 +1,5 @@
 class Challenge < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: { case_sensitive: false }
   validates :body, presence: true
   validates :difficulty, presence: true
   belongs_to :user
