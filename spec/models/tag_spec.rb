dca2140df5a147bc34f21eb977fb078335b8b4bb
Joel Guerra <joelaguerra1@gmail.com>
2017-07-30 12:03:05 -0700

    Write and pass Tag tests

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Tag, type: :model do
-  
+  let!(:tag) {FactoryGirl.create(:tag)}
   context 'validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_uniqueness_of(:name) }
