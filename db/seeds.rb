d008660a3b045ca92c37aae4df82da77218e405a
Joel Guerra <joelaguerra1@gmail.com>
2017-07-30 11:48:47 -0700

    updates seed file for new models

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,19 @@ user_data = [
 User.create(user_data)
 
 challenge_data = [
-  {title: 'Sum an array', input: '[1, 7, 9, 3, 4, 2, 6, -1]', output: '31', difficulty: 'Easy', user_id: 1,
-    body: 'Take an array of numbers (any length) and return a single number that is the sum of all the numbers'}, {title: 'Fizz Buzz', input: "1, 3, 4, 5, 15", output: "1, Fizz, 4, Buzz, FizzBuzz", difficulty: 'Medium', user_id: 1, body: 'Write a method that takes a single integer as an argument and returns Fizz is the number is divisible by 3, Buzz if the number is divisible by 5 and FizzBuzz if the number is divisible by both'}
+  {title: 'Sum an array', notes: 'Sample input: [1, 7, 9, 3, 4, 2, 6, -1] /n Sample output: 31', difficulty: 'Easy', user_id: 1, body: 'Take an array of numbers (any length) and return a single number that is the sum of all the numbers'}, {title: 'Fizz Buzz', notes: "Sample Input: 1, 3, 4, 5, 15 /n Sample output: 1, Fizz, 4, Buzz, FizzBuzz", difficulty: 'Medium', user_id: 1, body: 'Write a method that takes a single integer as an argument and returns Fizz is the number is divisible by 3, Buzz if the number is divisible by 5 and FizzBuzz if the number is divisible by both'}
 ]
 
 Challenge.create(challenge_data)
 
+ChallengeTag.create([
+  {challenge_id: 1, tag_id: 1},
+  {challenge_id: 1, tag_id: 2},
+  {challenge_id: 1, tag_id: 3},
+  {challenge_id: 2, tag_id: 1}
+  ])
+
+Tag.create([{name: 'algorithm'}, {name: 'data structures'}, {name: 'arrays'}])
 
 interview_data = [
   {challenge_id: rand(1..2), interviewer_id: 3, applicant_id: 5},
