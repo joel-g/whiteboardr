15e987044249f6413759ace8ab71ae553a3fde99
Joel Guerra & Jeremy Lee <pair+joel-g.pyrofist@devbootcamp.com>
2017-07-31 12:02:14 -0700

    Implement user#update action and user#edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.skip_password = true
+    @user.assign_attributes(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
 private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :username, :password, :password_confirmation)
