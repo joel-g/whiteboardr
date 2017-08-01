56d9a5a07a4bd2f15084d74072969088c598eda4
Joel Guerra <joelaguerra1@gmail.com>
2017-08-01 12:02:48 -0700

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
 
-# delete when done
-  get 'password_resets/create'
-  get 'password_resets/edit'
-  get 'password_resets/update'
-
-  resources :password_resets
-
+  resources :password_resets, except: [:destroy, :show]
   resources :challenges, only: [:index, :create, :new, :show]
   resources :interviews, except: [:destroy, :edit] do
     resources :feedbacks, only: [:create]
   end
-
   resources :users, except: [:destroy]
   resources :sessions, only: [:new, :create, :destroy]
 
