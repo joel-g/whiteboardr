1ff2577ca9d3abcf99521803abbb5953d6238e65
Joel Guerra <joelaguerra1@gmail.com>
2017-08-01 10:13:43 -0700

    adds PasswordResetController and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/create'
+
+  get 'password_resets/edit'
+
+  get 'password_resets/update'
+
   resources :challenges, only: [:index, :create, :new, :show]
   resources :interviews, except: [:destroy, :edit] do
     resources :feedbacks, only: [:create]
