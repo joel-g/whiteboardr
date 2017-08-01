cc49c75edf0cc0544ea16a115ba803e01b3c2ea5
Jeremy Lee <email": null>
2017-08-01 12:02:35 -0700

    Try a few more times before giving up on a unique token

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -27,7 +27,7 @@ class Interview < ActiveRecord::Base
     todays_interviews = TokenHelper.todays_interviews
     matching_interviews = TokenHelper.get_matches(todays_interviews, possible_token)
     fail_count = 0
-    while matching_interviews.count > 0 && fail_count < 4
+    while matching_interviews.count > 0 && fail_count < 10
       possible_token = TokenHelper.create_token(self.interviewer_id, self.applicant_id, self.challenge_id, Time.now)
       matching_interviews = TokenHelper.get_matches(todays_interviews, possible_token)
       fail_count += 1
