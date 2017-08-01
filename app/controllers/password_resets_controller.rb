e85cfc1a7bdd96e3022aec1490fb8bc5e384f005
Joel Guerra <joelaguerra1@gmail.com>
2017-08-01 10:18:57 -0700

    defines edit, update, create in PasswordResetsController

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,10 +1,36 @@
 class PasswordResetsController < ApplicationController
+  skip_before_filter :require_login
+
   def create
+     @user = User.find_by_email(params[:email])
+     @user.deliver_reset_password_instructions! if @user
+     redirect_to(root_path, :notice => 'Instructions have been sent to your registered email address.')
   end
 
   def edit
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(params[:id])
+
+    if @user.blank?
+      not_authenticated
+      return
+    end
   end
 
   def update
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(params[:id])
+
+    if @user.blank?
+      not_authenticated
+      return
+    end
+    @user.password_confirmation = params[:user][:password_confirmation]
+    if @user.change_password!(params[:user][:password])
+      redirect_to(root_path, :notice => 'Password was successfully updated.')
+    else
+      render :action => "edit"
+    end
   end
+
 end
