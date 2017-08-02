90562b8caa0aec7f89f747ebf10754622e5fc6e3
Joel Guerra <joelaguerra1@gmail.com>
2017-08-01 18:08:30 -0700

    Write challenges

---
+++ db/seed_challenges.rb
@@ -0,0 +1,38 @@
+challenge_data = [
+  {     title: 'Invert Values',
+         body: 'Write a function that given a set of numbers return the inverse of the numbers.',
+        notes: 'invert([1, 2, 3, 4, 5]) would return [ -1, -2, -3, -4, -5]\n invert([-2, 3, 1]) would return [2, -3, -1]\n invert([]) would return []\n You may assume all numbers are integers.',
+   difficulty: 'Easy',
+      user_id:  1,
+       },
+   {     title: 'Invert Values',
+          body: 'Create a function that will return a string that combines all of the letters of three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other',
+         notes: 'Input: aa, bb, cc, Output: abcabc\n You may assume all input strings are the same length.'
+    difficulty: 'Easy',
+       user_id:  1,
+        },
+
+]
+
+tag_data = [
+      { name: 'integers' },
+      { name: 'numbers '},
+      { name: 'arrays' },
+      { name: 'data structures'},
+      { name: 'fundamentals'}
+      { name: 'strings'}
+]
+
+challengetag_data = [
+  { challenge_id: 1, tag_id: 1 },
+  { challenge_id: 1, tag_id: 2 },
+  { challenge_id: 1, tag_id: 3 },
+  { challenge_id: 1, tag_id: 4 },
+  { challenge_id: 1, tag_id: 5 },
+  { challenge_id: 2, tag_id: 6 },
+]
+
+
+Challenge.create(challenge_data)
+Tag.create(tag_data)
+ChallengeTag.create(challengetag_data)
